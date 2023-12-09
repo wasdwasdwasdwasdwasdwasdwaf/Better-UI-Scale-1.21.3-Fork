9f44a7ee10ec344aa9750700f1e60698cbe983bb
Qendolin <32160662+Qendolin@users.noreply.github.com>
2023-12-09 18:14:19 +0100

    Update to 1.20.4

--- src/main/java/net/rosemods/betteruiscale/ConfigScreen.java
+++ src/main/java/net/rosemods/betteruiscale/ConfigScreen.java
@@ -22,7 +22,7 @@ public class ConfigScreen
 
     @Override
     protected void init() {
-        list = new OptionListWidget(client, width, height, 32, height - 32, 25);
+        list = new OptionListWidget(client, width, height-64, 32, 25);
         list.addAll(config.getOptions().values().toArray(SimpleOption[]::new));
         addSelectableChild(list);
         addDrawableChild(ButtonWidget.builder(ScreenTexts.DONE, button -> {
